a20012189ad91f508b1e46375c4adc03958017d3
5taZ <stasyfreak@gmail.com>
2026-01-31 00:08:44 +0300

    Create telegram.d.ts

---
+++ src/types/telegram.d.ts
@@ -0,0 +1,46 @@
+interface TelegramUser {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code?: string;
+  is_premium?: boolean;
+}
+
+interface TelegramWebApp {
+  initData: string;
+  initDataUnsafe: {
+    query_id?: string;
+    user?: TelegramUser;
+    auth_date?: number;
+    hash?: string;
+  };
+  ready: () => void;
+  expand: () => void;
+  close: () => void;
+  colorScheme: 'light' | 'dark';
+  themeParams: {
+    bg_color?: string;
+    text_color?: string;
+    hint_color?: string;
+    link_color?: string;
+    button_color?: string;
+    button_text_color?: string;
+  };
+  MainButton: {
+    text: string;
+    show: () => void;
+    hide: () => void;
+    onClick: (callback: () => void) => void;
+  };
+}
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: TelegramWebApp;
+    };
+  }
+}
+
+export {};
